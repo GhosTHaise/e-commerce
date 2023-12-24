87cfdd116c37f4346bc570d84868c047b7642571
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-12-24 23:04:37 +0300

    feat : build navbar

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,13 +3,21 @@ import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 
+import classes from "./index.module.scss"
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import HeaderMobileNav from '../MobileNav'
+
 const HeaderComponent = ({ header } : { header : Header  }) => {
   return (
-    <nav>
-        <Gutter>
+    <nav className={classes.header} >
+        <Gutter className={classes.wrap}>
             <Link href="/">
-              <img src="/logo-black.svg" alt="logo"  />
+              <Image src="/logo-black.svg" alt="logo" className={classes.logo} width={170} height={50}  />
             </Link>
+
+            <HeaderNav header={header} />
+            <HeaderMobileNav header={header} />
         </Gutter>
     </nav>
   )
