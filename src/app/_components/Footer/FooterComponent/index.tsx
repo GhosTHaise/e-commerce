2ec4d6750a85c96bc1c382c317245c064ac0bc1b
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-12-28 20:46:34 +0300

    feat : add footer inclusion

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,13 +1,43 @@
+"use client"
 import React from 'react'
 import { Footer } from '../../../../payload/payload-types'
 import classes from "./index.module.scss"
+import { inclusions, noHeaderFooterUrls } from '../../../constants'
+import { usePathname } from 'next/navigation'
+import { Gutter } from '../../Gutter'
+import Image from 'next/image'
 
 type FooterComponentProps = {
     footer : Footer
 }
 const FooterComponent = ({footer} : FooterComponentProps) => {
+  const pathname = usePathname()
   return (
-    <div>FooterComponent</div>
+    <footer className={noHeaderFooterUrls.includes(pathname) ? classes.hide : ""}>
+        <Gutter>
+            <ul className={classes.inclusions}>
+                {
+                  inclusions.map((inclusion) => (
+                    <li key={inclusion.title}>
+                      <Image 
+                        src={inclusion.icon}
+                        alt={inclusion.title}
+                        width={36}
+                        height={36}
+                        className={classes.icon}
+                      />
+                      <h5 className={classes.title}>
+                        {inclusion.title}
+                      </h5>
+                      <p>
+                        {inclusion.description}
+                      </p>
+                    </li>
+                  ))
+                }
+            </ul>
+        </Gutter>
+    </footer>
   )
 }
 
