afe4d0236a4493276b21e2f740a903500f395ae4
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-12-31 14:56:33 +0300

    feat : make recoverForm like loginform

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -1,17 +1,49 @@
 import React from 'react'
 import { Metadata } from 'next'
 
-import { Gutter } from '../../_components/Gutter'
 import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword} >
+      <div className={classes.heroImg}>
+          <Link href="/">
+              <Image
+                src="/logo-black.svg"
+                alt='logo'
+                width={250}
+                height={23}
+                className={classes.logo}
+              />
+          </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+
+          <div className={classes.formTitle}>
+              <h3>
+                Welcome
+              </h3>
+              <Image 
+                  src="/assets/icons/hand.png"
+                  alt='hand'
+                  width={30}
+                  height={30}
+              />
+          </div>
+
+          <p>Please login here</p>
+
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
