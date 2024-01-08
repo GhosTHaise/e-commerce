81a6b6541c93d0f0aa97956a6bcdcf239d193810
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-08 21:04:45 +0300

    feat : custom module components

---
+++ src/app/_components/Categories/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Category } from '../../../payload/payload-types'
+
+type CategoriesProps = {
+    categories : Category[]
+}
+const Categories = ({categories} : CategoriesProps) => {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default Categories
