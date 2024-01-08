a196f287e78bcdd944eb8d097b8dfa2f8aa9ee00
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-08 21:10:47 +0300

    feat : categories - classes ,title

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Category } from '../../../payload/payload-types'
+import classes from "./index.module.scss"
 
 type CategoriesProps = {
     categories : Category[]
 }
 const Categories = ({categories} : CategoriesProps) => {
   return (
-    <section>
-
+    <section className={classes.container}>
+        <div className={classes.titleWrapper}>
+            <h3>
+                Shop by Categories
+            </h3>
+        </div>
     </section>
   )
 }
