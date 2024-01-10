5c49f99a8ef22edcdf622fc101e3ee9bdf73fb96
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-10 20:21:43 +0300

    feat : filter providers

---
+++ src/app/_providers/Filter/index.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React, { useState } from "react";
+import { createContext, useContext } from "react"
+
+export const INITIAL_FILTER_DATA = {
+    categoryFilters : [],
+    setCategoryFilters : () => [],
+    sort : "",
+    setSort : () => ""
+}
+
+const FilterContext = createContext(INITIAL_FILTER_DATA);
+
+export const FilterProvider = ({ children} : {children : React.ReactNode}) => {
+    const [categoryFilters, setCategoryFilters] = useState([]);
+    const [sort, setSort] = useState<string>("")
+    return(
+        <FilterContext.Provider value={{
+            categoryFilters ,
+            setCategoryFilters,
+            sort,
+            setSort
+        }}>
+            {children}
+        </FilterContext.Provider>
+    )
+}
+
+export const useFilter = () => useContext(FilterContext)
