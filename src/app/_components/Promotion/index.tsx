f1758131ea57e58ed9290d60f81441be8f4e3477
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-11 21:16:34 +0300

    feat : promotion stats box

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -3,7 +3,24 @@ import classes from "./index.module.scss"
 
 const Promotion = () => {
   return (
-    <div>index</div>
+    <section className={classes.promotion}>
+        <div className={classes.textBox}>
+            <h3>
+                Deals of the Month
+            </h3>
+            <p>
+              Get ready for a shopping experience like never before with our Deals of the Month! Every
+              purchase comes with exclusive perks and offers, making this month a celebration of savvy
+              choices and amazing deals. Don't miss out! 🎁🛒
+            </p>
+            <ul className={classes.stats}>
+                <li className={classes.statBox}>
+                    <h4>5</h4>
+                    <p>Days</p>
+                </li>
+            </ul>
+        </div>
+    </section>
   )
 }
 
