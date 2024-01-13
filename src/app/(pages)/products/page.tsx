6b44dde1f07ad094af2e7bc3e84c912dfabea85a
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-13 19:11:47 +0300

    feat : fIlters styles

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import classes from "./index.module.scss";
+import { Gutter } from '../../_components/Gutter';
+import Filters from './Filters';
 
 const Products = () => {
   return (
-    <div>page</div>
+    <div className={classes.container}>
+        <Gutter className={classes.products} >
+                <Filters />
+        </Gutter>
+    </div>
   )
 }
 
