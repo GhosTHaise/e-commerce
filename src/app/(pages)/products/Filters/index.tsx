a4ba8f8f5bcc82724d56d73e2ea035b14e466005
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-14 21:21:29 +0300

    feat : list categories on custom checkbox

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+"use client"
+import React from 'react';
 
-const Filters = () => {
+import classes from "./index.module.scss";
+import { useFilter } from '../../../_providers/Filter';
+import { Category } from '../../../../payload/payload-types';
+
+const Filters = ({categories} : {categories : Category[]}) => {
+  const { categoryFilters,setCategoryFilters,sort,setSort} = useFilter();
+
+  const handleCategories = (categoryId : string) => {
+
+  }
+
   return (
-    <div>index</div>
+    <div className={classes.filters }>
+      <h6 className={classes.title}>
+          Product Categories
+      </h6>
+      <div className={classes.categories}>
+          {
+            categories.map((category : Category) => {
+              const isSelected = false;
+              return <div></div>
+            })
+          }
+      </div>
+    </div>
   )
 }
 
