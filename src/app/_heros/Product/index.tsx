2e7c53b47739d95dd0331adf6dbeb9501f08a517
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-17 21:58:27 +0300

    feat : adjust product details elements positions

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import Link from 'next/link'
 
-import { Product } from '../../../payload/payload-types'
+import { Category, Product } from '../../../payload/payload-types'
 import { AddToCartButton } from '../../_components/AddToCartButton'
 import { Gutter } from '../../_components/Gutter'
 import { Media } from '../../_components/Media'
@@ -23,47 +23,50 @@ export const ProductHero: React.FC<{
   } = product
 
   return (
-      <Gutter className={classes.productHero}>
-          <div className={classes.mediaWrapper}>
-            {!metaImage && <div className={classes.placeholder}>No image</div>}
-            {metaImage && typeof metaImage !== 'string' && (
-              <Media imgClassName={classes.image} resource={metaImage} fill />
-            )}
-          </div>
-    
-          <div className={classes.details}>
-              <h3 className={classes.title}>
-                {title}
-              </h3>
-          </div>
+    <Gutter className={classes.productHero}>
+      <div className={classes.mediaWrapper}>
+        {!metaImage && <div className={classes.placeholder}>No image</div>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </div>
+
+      <div className={classes.details}>
+        <h3 className={classes.title}>
+          {title}
+        </h3>
+
+        <div className={classes.categoryWrapper}>
           <div className={classes.categories}>
             {categories?.map((category, index) => {
-              const { title: categoryTitle } = category
+              const { title: categoryTitle } : any = category
 
               const titleToUse = categoryTitle || 'Untitled category'
 
               const isLast = index === categories.length - 1
 
               return (
-                <Fragment key={index}>
+                <p key={index} className={classes.category}>
                   {titleToUse}
                   {!isLast && <Fragment>, &nbsp;</Fragment>}
-                </Fragment>
+                </p>
               )
             })}
           </div>
-          
-          <div>
-            <p className={classes.description}>
-              {`${description ? `${description} ` : ''}To edit this product, `}
-              <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}>
-                navigate to the admin dashboard
-              </Link>
-              {'.'}
-            </p>
-          </div>
-          <Price product={product} button={false} />
-          <AddToCartButton product={product} className={classes.addToCartButton} />
-      </Gutter>
+        </div>
+      </div>
+
+      <div>
+        <p className={classes.description}>
+          {`${description ? `${description} ` : ''}To edit this product, `}
+          <Link href={`${process.env.NEXT_PUBLIC_SERVER_URL}/admin/collections/products/${id}`}>
+            navigate to the admin dashboard
+          </Link>
+          {'.'}
+        </p>
+      </div>
+      <Price product={product} button={false} />
+      <AddToCartButton product={product} className={classes.addToCartButton} />
+    </Gutter>
   )
 }
