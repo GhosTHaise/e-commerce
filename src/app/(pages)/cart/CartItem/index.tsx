fcdbf1714aa021b98d4d1d0655be98f73165aa01
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-01-29 19:14:40 +0300

    feat : start to build product cart

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,6 +1,10 @@
 "use client"
 import React, { useState } from 'react'
 import classes from "./index.module.scss"
+import Link from 'next/link'
+import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
+import Image from 'next/image'
 
 const CartItem = ({
   product,
@@ -11,9 +15,33 @@ const CartItem = ({
 }) => {
 
   const [quantity, setQuantity] = useState(qty)
+
+  const decrementQty = () => {}
+  const incrementQty = () => {}
+  const enterQty = () => {}
+
   return (
     <li className={classes.item} key={title}>
-        <h6>{title}</h6>
+        <Link 
+          href={`/products/${product.slug}`}
+          className={classes.mediaWrapper}
+          >
+            {
+              !metaImage && <span>No Image</span> 
+            }
+            {
+              metaImage && typeof metaImage !== "string" && (
+                <Media
+                  className={classes.media}
+                  imgClassName={classes.image}
+                  resource={metaImage}
+                  fill
+                />
+              )
+            }
+        </Link>
+
+        
     </li>
   )
 }
